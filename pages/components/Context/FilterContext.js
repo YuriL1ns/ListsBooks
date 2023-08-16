a4c5586697cf2168a371d8e988736951876dc660
YuriL1ns <yuri.lins@outlook.com>
2023-08-16 02:18:07 -0300

    update dev

--- pages/components/Context/FilterContext.js
+++ pages/components/Context/FilterContext.js
@@ -1,21 +1,30 @@
 import React, { createContext, useContext, useState } from "react";
 
+// Criando o contexto
 const FilterContext = createContext();
 
+// Hook personalizado para usar o contexto
 export const useFilterContext = () => useContext(FilterContext);
 
+// Componente de provedor do contexto
 export const FilterProvider = ({ children }) => {
-  const [selectedThemes, setSelectedThemes] = useState("");
+  const [selectedThemes, setSelectedThemes] = useState([]);
 
   const updateSelectedThemes = (themes) => {
     setSelectedThemes(themes);
   };
 
+  // Criando o valor do contexto
+  const contextValue = {
+    selectedThemes,
+    updateSelectedThemes,
+  };
+
   return (
-    <FilterContext.Provider value={{ selectedThemes, updateSelectedThemes }}>
+    <FilterContext.Provider value={contextValue}>
       {children}
     </FilterContext.Provider>
   );
 };
 
-export default FilterContextPage;
+export default FilterContext;
