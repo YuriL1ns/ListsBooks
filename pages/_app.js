71631327ca2ae5a8360695605d1919d77bbefefa
YuriL1ns <yuri.lins@outlook.com>
2023-08-16 08:53:31 -0300

    update _app.js

--- pages/_app.js
+++ pages/_app.js
@@ -1,12 +1,12 @@
 import React from "react";
 import "../styles/globals.css";
 import { FilterProvider } from "./components/Context/FilterContext";
-import SearchBar from "./components/SearchBar/SearchBar";
+import Home from "./index";
 
 const App = () => {
   return (
     <FilterProvider>
-      <SearchBar />
+      <Home />
     </FilterProvider>
   );
 };
